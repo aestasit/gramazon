3bfdd6f186197c78989167aec20f46ca21690e9f
Andrey Adamovich <andrey@aestasit.com>
2013-04-18 11:17:08 +0200

    minor formatting

--- src/main/groovy/com/aestasit/cloud/aws/gradle/InstanceStateUtils.groovy
+++ src/main/groovy/com/aestasit/cloud/aws/gradle/InstanceStateUtils.groovy
@@ -5,7 +5,7 @@ import groovy.json.JsonSlurper
 
 /**
  * Utility methods for reading/writing Amazon EC2 instance state data.
- * 
+ *
  * @author Aestas/IT
  *
  */
@@ -13,31 +13,30 @@ class InstanceStateUtils {
 
   /**
    * Deserialize the InstanceState pojo from JSON.
-   * 
-   * @param f the file where the JSON file is
-   * @return a deserialized InstanceState
+   *
+   * @param f the file where the JSON file is.
+   * @return a deserialized InstanceState.
    */
   static InstanceState getInstanceState(File f) {
     def s = new JsonSlurper().parseText(f.text)
     return new InstanceState(s.instanceState.name,
-                             s.instanceState.host,
-                             s.instanceState.instanceId)
+    s.instanceState.host,
+    s.instanceState.instanceId)
   }
-  
+
   /**
-   * Save the InstanceState pojo to a Json file.
+   * Save the InstanceState POJO to a JSON file.
    *
-   * @param f the file where to save serialize the pojo
-   * @param state the pojo to serialize
+   * @param f the file where to save serialize the POJO.
+   * @param state the POJO to serialize.
    */
-   static void setInstanceState(File f, InstanceState state) {
+  static void setInstanceState(File f, InstanceState state) {
     JsonBuilder builder = new JsonBuilder()
     builder.instanceState(
         name: state.name,
         host: state.host,
         instanceId: state.instanceId
-    )
+        )
     f.withWriter { it << builder.toPrettyString() }
   }
-
 }
