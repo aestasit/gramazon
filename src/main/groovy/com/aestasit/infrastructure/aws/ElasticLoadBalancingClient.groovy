6fb6f79a113e9cf684e3e893bbcddc7c84292a46
Kyle Boon <kyle.f.boon@gmail.com>
2014-11-11 14:50:19 -0600

    Upgraded to latest amazon sdk

--- src/main/groovy/com/aestasit/infrastructure/aws/ElasticLoadBalancingClient.groovy
+++ src/main/groovy/com/aestasit/infrastructure/aws/ElasticLoadBalancingClient.groovy
@@ -1,7 +1,7 @@
 package com.aestasit.infrastructure.aws
 
 import com.aestasit.infrastructure.aws.request.ElasticLoadBalancingRequestBuilder
-import com.amazonaws.auth.SystemPropertiesCredentialsProvider 
+import com.amazonaws.auth.SystemPropertiesCredentialsProvider
 import com.amazonaws.regions.Region
 import com.amazonaws.services.elasticloadbalancing.model.DeregisterInstancesFromLoadBalancerResult
 import com.amazonaws.services.elasticloadbalancing.model.Instance
@@ -12,28 +12,28 @@ import com.amazonaws.services.elasticloadbalancing.AmazonElasticLoadBalancingCli
 import com.amazonaws.services.elasticloadbalancing.model.DeregisterInstancesFromLoadBalancerRequest
 
 class ElasticLoadBalancingClient {
-	
+
 	final AmazonElasticLoadBalancingClient loadBalancingClient
 	final String loadBalancerName
-	
+
 	ElasticLoadBalancingClient(String loadBalancerName, String region = 'eu-west-1',
 		AmazonElasticLoadBalancingClient amazonLoadBalancingClient = new AmazonElasticLoadBalancingClient(new SystemPropertiesCredentialsProvider())) {
 		this.loadBalancingClient = amazonLoadBalancingClient
 		this.loadBalancerName = loadBalancerName
-		
-		Region loadBalancerRegion = new Region(region)
+
+		Region loadBalancerRegion = new Region(region, null)
 		loadBalancerRegion.serviceEndpoints.put('elasticloadbalancing', "elasticloadbalancing."+region+".amazonaws.com")
 		this.loadBalancingClient.setRegion(loadBalancerRegion)
 	}
-	
+
 	RegisterInstancesWithLoadBalancerResult addInstancesToLoadBalancer(String[] intancesIds, ElasticLoadBalancingRequestBuilder requestBuilder = new ElasticLoadBalancingRequestBuilder()) {
 		def registerRequest = requestBuilder.withLoadBalancerName(loadBalancerName).withInstancesIds(intancesIds).buildRegisterInstancesRequest()
 		loadBalancingClient.registerInstancesWithLoadBalancer(registerRequest)
 	}
-	
+
 	DeregisterInstancesFromLoadBalancerResult removeInstancesFromLoadBalancer(String[] intancesIds, ElasticLoadBalancingRequestBuilder requestBuilder = new ElasticLoadBalancingRequestBuilder()) {
 		def deregisterRequest = requestBuilder.withLoadBalancerName(loadBalancerName).withInstancesIds(intancesIds).buildDeregisterInstancesRequest();
 		loadBalancingClient.deregisterInstancesFromLoadBalancer(deregisterRequest)
 	}
-	
+
 }
